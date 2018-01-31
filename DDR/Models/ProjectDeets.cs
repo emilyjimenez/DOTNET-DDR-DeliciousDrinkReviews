548042e1679a6bc927d3773dd3f5f507b36dac39
Emily Wells Jimenez <emwellspdx@gmail.com>
2018-01-31 10:13:55 -0800

    build out API callback function for getting GH projects, build controller, now to finish up the view.

---
+++ DDR/Models/ProjectDeets.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using RestSharp;
+using RestSharp.Authenticators;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace DDR.Models
+{
+    public class Project
+    {
+
+        public static async Task<List<Project>> GetProjects()
+        {
+            var client = new RestClient("https://api.github.com");
+
+            var request = new RestRequest("users/emilyjimenez/starred", Method.GET);
+            request.AddHeader("User-Agent", "emilyjimenez");
+            request.AddParameter("direction", "asc");
+
+            var response = new RestResponse()
+                Task.Run(async () =>
+            {
+                response = await GetResponseContentAsync(client, request) as RestResponse;
+            }).Wait();
+
+            JArray jsonResponse = JsonConvert.DeserializeObject<JArray>(response.Content);
+                        
+                        
+                        )
+        }
+    }
+}
